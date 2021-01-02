2014cb4ec86cae2c32afc5c74184d9111c72b6f5
HusseinAdel <hussein.adel29@yahoo.com>
2021-01-02 17:31:30 +0200

    footer styles

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,6 +7,13 @@ const Footer = props =>{
     return(
       <div className="footer-img">
             <img src={glassImage}></img>
+            <div class="text-block">
+                <h1><i>THE BEST WHISKIES FROM SCOTLAND</i></h1>
+                <p>Ever wanted to get into scotch but don't know where to start? We've reviewed 
+                    some of the most popular whiskies and break them down by their tasting notes.
+                </p>
+            </div>
+
       </div>
     );
 };
