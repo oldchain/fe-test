04e8ceae473492f5bf8616325693e5bc38b573f6
oldchain <hussein.adel29@yahoo.com>
2021-01-04 16:38:35 +0200

    fixing footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,7 +4,7 @@ import './styles.scss';
 const Footer = props =>{  
     
     return(
-      <div href={props.article.url} target="_blank" className="footer-img">
+      <a href={props.article.url} target="_blank" className="footer-img">
             <img src={require('./../../assets/' + props.article.img).default} alt={props.article.title}></img>
             <div className="text-block">
                 <h1><i>{props.article.title.toUpperCase()}</i></h1>
@@ -12,7 +12,7 @@ const Footer = props =>{
                 </p>
             </div>
 
-      </div>
+      </a>
     );
 };
 
